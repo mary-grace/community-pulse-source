9f77c3cc9f494821ca55a96d672599e66343f93f
Jason Hand <jasonhand@users.noreply.github.com>
2020-05-22 07:26:53 -0600

    This is my new post

--- CreateNewPost.sh
+++ CreateNewPost.sh
@@ -2,9 +2,11 @@
 
 # Tell user we are downloading the markdown file and then wget to obtain
 echo "Downloading episode markdown"
-wget https://jhandcdn.blob.core.windows.net/blob/hug_markdown_files/50-The-Year-of-Automation-6.md -P /content/episode/
+wget https://jhandcdn.blob.core.windows.net/blob/hug_markdown_files/50-The-Year-of-Automation-6.md
 # Git Steps
-echo "Sync'ing with repo"
+# Move file to episodes directory
+mv 50-The-Year-of-Automation-6.md content/episode/
+#echo "Sync'ing with repo"
 git add .
 git commit -m "This is my new post"
 git push origin master

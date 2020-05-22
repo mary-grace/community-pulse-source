614502e30e0b753431f92487bff2584d27e61a6d
Jason Hand <jasonhand@users.noreply.github.com>
2020-05-22 06:41:57 -0600

    This is my new post

--- CreateNewPost.sh
+++ CreateNewPost.sh
@@ -3,7 +3,7 @@
 # Tell user we are downloading the markdown file and then wget to obtain
 echo "Downloading episode markdown"
 wget
-https://jhandcdn.blob.core.windows.net/blob/hug_markdown_files/This-Should-Rename-The-Script-2020-05-22T12:36:28.15Z.md?sv=2018-03-28&sr=b&sig=%2F1IvpSg23%2B%2BtNkf%2FfEZoMwZNfdFj0W3A4I6xFSTfN5s%3D&se=2020-05-23T12%3A36%3A38Z&sp=r
+https://jhandcdn.blob.core.windows.net/blob/hug_markdown_files/50-The-Year-of-Automation-2020-05-22T12:40:36.047Z.md?sv=2018-03-28&sr=b&sig=XB9Fn10NKGO0Myuksrnl8B8%2Ff6Hk0CKz9MBf8MYrPhI%3D&se=2020-05-23T12%3A40%3A57Z&sp=r
 -P /content/episode/
 # Git Steps
 echo "Sync'ing with repo"

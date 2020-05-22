306dc8bb38097906c7fe78ef8df651ceb92de1bf
Jason Hand <jasonhand@users.noreply.github.com>
2020-05-22 08:14:57 -0600

    This is my new post

--- CreateNewPost.sh
+++ CreateNewPost.sh
@@ -3,14 +3,15 @@
 # Tell user we are downloading the markdown file and then wget to obtain
 echo "Downloading episode markdown"
 echo "#####"
-wget "https://jhandcdn.blob.core.windows.net/blob/hugo/55-Best-The-Year-of-Automation-5.md"
+wget
+"https://jhandcdn.blob.core.windows.net/blob/hugo/This-Might-Be-Best-Episode-6.md"
 echo "#####"
-mv 55-Best-The-Year-of-Automation-5.md content/episode/
+mv This-Might-Be-Best-Episode-6.md content/episode/
 # Git Steps
 #echo "Sync'ing with repo"
-#git add .
-#git commit -m "This is my new post"
-#git push origin master
+git add .
+git commit -m "This is my new post"
+git push origin master
 echo "######################"
 echo "This has completed"
 echo "#######################"
